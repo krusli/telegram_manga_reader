5e490d665d4a1256b1d55a1ef9189ee18b92fa97
krusli <kenneth@Kenneths-Air.mshome.net>
2016-03-16 23:37:00 +1100

    Added multiprocessing support to /openchapter

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-import requests, json, csv, random
-
-# ----- operating variables -----
-# get last_update, so bot can request messages after a certain update ID.
-try:
-    with open('last_update.txt', 'r') as f:
-        last_update = int(f.readline().strip())
-except FileNotFoundError:
-    last_update = 0
+import requests, json, csv, random, os, multiprocessing
 
 # URL and auth token for bot requests
 token = '' # enter the API token from BotFather here
@@ -40,12 +32,23 @@ send = send_class()
 
 def download_file_to_tempfile(url):
     r = requests.get(url, stream=True)
-    with open('tempfile.jpg', 'wb') as f:
+
+    # if file exists, create another file as to not disrupt other processes
+    filename = 'tempfile'
+    if filename + '.jpg' in os.listdir('.'):
+        while True:
+            filename += str(random.randint(1,10))
+            if filename + '.jpg' not in os.listdir('.'):
+                break
+    filename += '.jpg'
+
+    with open(filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
                 f.flush()
-    return
+
+    return filename
 
 # ----- MangaEden reader -----
 class manga_class:
@@ -102,8 +105,8 @@ class manga_class:
         pages = json.loads(requests.get('http://www.mangaeden.com/api/chapter/' + chapter_ID + '/').text)  # list of pages of current manga
         for entry in sorted(pages['images']):
             page_ID = entry[1]
-            download_file_to_tempfile('https://cdn.mangaeden.com/mangasimg/' + page_ID)
-            send.send_photo(chatid, 'tempfile.jpg')
+            filename = download_file_to_tempfile('https://cdn.mangaeden.com/mangasimg/' + page_ID)
+            send.send_photo(chatid, filename)
         return
 
 manga = manga_class()
@@ -116,58 +119,72 @@ class message:
 class inline_query:
     pass
 
-while True:
+def main():
+    # ----- operating variables -----
+    # get last_update, so bot can request messages after a certain update ID.
     try:
-        get_updates = json.loads(requests.get(url + 'getUpdates', dict(offset=last_update)).text)
-    except ConnectionError:
-        pass
+        with open('last_update.txt', 'r') as f:
+            last_update = int(f.readline().strip())
+    except FileNotFoundError:
+        last_update = 0
 
-    for update in get_updates['result']:
-        if last_update < update['update_id']:
-            last_update = update['update_id']
+    pool = multiprocessing.Pool(processes = 4)
 
-            with open('last_update.txt', 'w') as f: # write last_update to file
-                f.write(str(last_update))
+    while True:
+        try:
+            get_updates = json.loads(requests.get(url + 'getUpdates', dict(offset=last_update)).text)
+        except ConnectionError:
+            pass
 
-            if 'message' in update.keys(): # check if message or inline_query
-                message.message_id = update['message']['message_id']
-                message.chat_id = update['message']['chat']['id']
-                message.sender = update['message']['from']
+        for update in get_updates['result']:
+            if last_update < update['update_id']:
+                last_update = update['update_id']
 
-                if 'text' in update['message'].keys():
-                    message.text = update['message']['text']
-                    try:
-                        if message.text.startswith('/status'):
-                            send.message(message.chat_id, "I'm up and running!")
+                with open('last_update.txt', 'w') as f: # write last_update to file
+                    f.write(str(last_update))
 
-                        if message.text.startswith('/latestchapter'):
-                            try:
-                                syntax_test = message.text.split()[1]
-                                message.text = message.text[15:]
-                                send.message(message.chat_id, manga.latest_chapter(message.text))
-                            except IndexError:
-                                send.message_reply(message.chat_id, "Please use the correct format for the function. Example: /latestchapter gintama", message.message_id)
-                        if message.text.startswith('/listchapters'):
-                            try:
-                                syntax_test = message.text.split()[1]
-                                message.text = message.text[14:]
-                                for sublist in manga.list_chapters(message.text):
-                                    send.message(message.chat_id, '\n'.join(sublist))
-                            except IndexError:
-                                send.message_reply(message.chat_id, "Please use the correct format for the function. Example: /listchapters gintama", message.message_id)
-                        if message.text.startswith('/openchapter'):
-                            try:
-                                words = message.text.split()[1:]
-                                syntax_test = words[1]
-                                manga.open_manga_chapter(' '.join(words[0:-1]), words[-1], message.chat_id, message.message_id)
-                            except IndexError:
-                                send.message_reply(message.chat_id, 'Please use the correct format for /openchapter.\nExample: /openchapter gintama 1', message.message_id)
+                if 'message' in update.keys(): # check if message or inline_query
+                    message.message_id = update['message']['message_id']
+                    message.chat_id = update['message']['chat']['id']
+                    message.sender = update['message']['from']
+
+                    if 'text' in update['message'].keys():
+                        message.text = update['message']['text']
+                        try:
+                            if message.text.startswith('/status'):
+                                send.message(message.chat_id, "I'm up and running!")
+
+                            if message.text.startswith('/latestchapter'):
+                                try:
+                                    syntax_test = message.text.split()[1]
+                                    message.text = message.text[15:]
+                                    send.message(message.chat_id, manga.latest_chapter(message.text))
+                                except IndexError:
+                                    send.message_reply(message.chat_id, "Please use the correct format for the function. Example: /latestchapter gintama", message.message_id)
+                            if message.text.startswith('/listchapters'):
+                                try:
+                                    syntax_test = message.text.split()[1]
+                                    message.text = message.text[14:]
+                                    for sublist in manga.list_chapters(message.text):
+                                        send.message(message.chat_id, '\n'.join(sublist))
+                                except IndexError:
+                                    send.message_reply(message.chat_id, "Please use the correct format for the function. Example: /listchapters gintama", message.message_id)
+                            if message.text.startswith('/openchapter'):
+                                try:
+                                    words = message.text.split()[1:]
+                                    syntax_test = words[1]
+                                    pool.apply_async(manga.open_manga_chapter, (' '.join(words[0:-1]), words[-1], message.chat_id, message.message_id))
+                                except IndexError:
+                                    send.message_reply(message.chat_id, 'Please use the correct format for /openchapter.\nExample: /openchapter gintama 1', message.message_id)
 
 
-                    except SystemExit:
-                        sys.exit(0)
-                    except ConnectionError:
-                        pass
+                        except SystemExit:
+                            sys.exit(0)
+                        except ConnectionError:
+                            pass
 
-            elif 'inline_query' in update.keys():
-                pass
+                elif 'inline_query' in update.keys():
+                    pass
+
+if __name__ == "__main__":
+    main()
